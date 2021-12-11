336b6163bf60d770b2b41bc0c38d6d4f966de7bc
daniele <danivilarez@hotmail.com>
2021-12-11 14:56:08 -0300

    rota GetAll alterada

--- DELIVERY_DA_BELEZA/src/routes/parceirosRoutes.js
+++ DELIVERY_DA_BELEZA/src/routes/parceirosRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/parceirosController")
 
-router.get("/", controller.getAll )
+router.get("/all", controller.getAll )
 //router.post("/", controller.postColaboradora)
 
 
